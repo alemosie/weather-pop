202987843a2fb26381e53eb59522a435c08c2af9
alemosie <alexandra.siega@gmail.com>
2016-07-04 10:27:26 -0400

    add listener to submit

--- app/assets/javascripts/open_weather_adapter.js
+++ app/assets/javascripts/open_weather_adapter.js
@@ -1,1 +1,28 @@
-// http://api.openweathermap.org/data/2.5/weather?q=newyork&APPID=7f67d8af0112f3fa0f3223bed93e584d
+// http://api.openweathermap.org/data/2.5/weather?q=newyork&APPID=APIKEY
+
+$(document).ready(function(){
+  $('#submit').click(createQuery);
+});
+
+function createQuery(event){
+  event.preventDefault();
+  event.stopPropagation();
+
+  var location = $("#location").val()
+  var api = new OpenWeatherAdapter(location);
+
+  $("#location").empty();
+  alert(location);
+  // api.getWeatherData();
+}
+
+function OpenWeatherAdapter(location) {
+  this.location = location;
+  this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=APIKEY";
+}
+
+function getWeatherData() {
+  $.getJSON(this.url, function(response) {
+    var weather = response.weather[0].description
+  }).appendTo("#cur-weather");
+}
